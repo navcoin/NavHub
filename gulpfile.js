275c5de44bcb546cd0b17c9ef4a2ef45f522bd8b
red010b37 <red010b37@users.noreply.github.com>
2019-06-11 14:38:04 +1200

    Adds webp processor

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+const imagemin = require('imagemin-keep-folder');
+const imageminWebp = require("imagemin-webp");
 
 gulp.task('sass', function () {
   buildSass()
@@ -8,6 +10,28 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   buildSass()
   gulp.watch('./themes/navhub-v2/static/css/sass/**/*.scss', ['sass']);
+});
+
+
+gulp.task("exportWebP", function() {
+
+    imagemin(['./**/*.{jpg,png}'], {
+        use: [
+            imageminWebp({quality: 85})
+        ]
+    });
+
+    // let src = "./**/*.png"; // Where your PNGs are coming from.
+    // let dest = "dist/images"; // Where your WebPs are going.
+    //
+    // return gulp.src(src)
+    //     .pipe(imagemin([
+    //         webp({
+    //             quality: 85
+    //         })
+    //     ]))
+    //     .pipe(extReplace(".webp"))
+    //     .pipe(gulp.dest(src));
 });
 
 function buildSass() {
